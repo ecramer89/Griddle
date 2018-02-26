8c7eaff6c08ab443d586344c02cd4752aba1b98a
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2018-02-25 16:40:07 -0800

    level select screen; level 2

---
+++ Assets/Scripts/LevelSelectButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LevelSelectButton : MonoBehaviour {
+
+	public int level;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnMouseDown(){
+
+		Debug.Log("clicked");
+
+		SceneManager.LoadScene(String.Format("Level{0}", level));
+
+	}
+}
